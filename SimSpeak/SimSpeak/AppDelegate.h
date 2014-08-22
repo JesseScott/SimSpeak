9688aeb74fd4e8fd265885bdf1ff25851a94c6ca
Jesse Scott <jessecolinscott@gmail.com>
2014-08-21 21:41:44 -0700

    base xcode project

---
+++ SimSpeak/SimSpeak/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimSpeak
+//
+//  Created by Jesse Scott on 2014-08-21.
+//  Copyright (c) 2014 jesses.co.tt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimSpeak/SimSpeakTests/SimSpeakTests.m
@@ -0,0 +1,34 @@
+//
+//  SimSpeakTests.m
+//  SimSpeakTests
+//
+//  Created by Jesse Scott on 2014-08-21.
+//  Copyright (c) 2014 jesses.co.tt. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SimSpeakTests : XCTestCase
+
+@end
+
+@implementation SimSpeakTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
